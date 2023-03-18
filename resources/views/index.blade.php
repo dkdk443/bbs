4e4daced094b3d078f38c5b98f9db3681333f033
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2023-03-18 15:09:27 +0900

    スレッド一覧画面の画面実装

---
+++ resources/views/index.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <title>えぬちゃんねる</title>
+</head>
+
+<body>
+    <header mb-4>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="" href="">
+                    <img src="nch-logo.png" width="280px">
+                </a>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+                    data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Home
+                    </a>
+
+                    <a class="navbar-item">
+                        Documentation
+                    </a>
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button is-primary">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a class="button is-light">
+                                Log in
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="columns">
+        {{-- <div class="column is-one-fifth mr-5">
+            <aside class="menu block">
+                <p class="menu-label">
+                    General
+                </p>
+                <ul class="menu-list">
+                    <li><a>Dashboard</a></li>
+                    <li><a>Customers</a></li>
+                </ul>
+                <p class="menu-label">
+                    Administration
+                </p>
+                <ul class="menu-list">
+                    <li><a>Team Settings</a></li>
+                    <li>
+                        <a class="is-active">Manage Your Team</a>
+                        <ul>
+                            <li><a>Members</a></li>
+                            <li><a>Plugins</a></li>
+                            <li><a>Add a member</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Invitations</a></li>
+                    <li><a>Cloud Storage Environment Settings</a></li>
+                    <li><a>Authentication</a></li>
+                </ul>
+                <p class="menu-label">
+                    Transactions
+                </p>
+                <ul class="menu-list">
+                    <li><a>Payments</a></li>
+                    <li><a>Transfers</a></li>
+                    <li><a>Balance</a></li>
+                </ul>
+            </aside>
+        </div> --}}
+        <div class="container column">
+            <div class="block">
+                <div class="box">
+                    <div class="section">
+                        <i class="fa-solid fa-ellipsis"></i>
+                        <h4 class="title is-4 mt-5">これはｗｗｗ重大なミスｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ</h4>
+                        <h5 class="subtitle is-5 has-text-right">2023-01-02</h5>
+                        <div class="tags are-medium">
+                            <span class="tag">All</span>
+                            <span class="tag">Medium</span>
+                            <span class="tag">Size</span>
+                        </div>
+                    </div>
+                    <div class="content section">
+                        <div class="my-6">
+                            <h5 class="subtitle is-6">1 ：以下、名無しにかわりましてVIPがお送りします：2011/03/05(土) 13:41:42.31 ID:OAFBoFRd0
+                            </h5>
+                            <p class="is-size-5">草
+                            </p>
+                        </div>
+                        <div class="my-6">
+                            <h5 class="subtitle is-6">2 ：
+                                <span style="font-weight: 600">以下、名無しにかわりましてVIPがお送りします</span>
+                                ：2011/03/05(土) 13:41:42.31 ID:OAFBoFRd0
+                            </h5>
+                            <p class="is-size-5">勢いがすごい
+                            </p>
+                        </div>
+                        <div class="my-1">
+                            <a href="#" class="btn btn-primary">続きを読む</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="section">
+                        <i class="fa-solid fa-ellipsis"></i>
+                        <h4 class="title is-4 mt-5">これはｗｗｗ重大なミスｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ</h4>
+                        <h5 class="subtitle is-5 has-text-right">2023-01-02</h5>
+                        <div class="tags are-medium">
+                            <span class="tag">All</span>
+                            <span class="tag">Medium</span>
+                            <span class="tag">Size</span>
+                        </div>
+                    </div>
+                    <div class="content section">
+                        <div class="my-6">
+                            <h5 class="subtitle is-6">1 ：以下、名無しにかわりましてVIPがお送りします：2011/03/05(土) 13:41:42.31 ID:OAFBoFRd0
+                            </h5>
+                            <p class="is-size-5">草
+                            </p>
+                        </div>
+                        <div class="my-6">
+                            <h5 class="subtitle is-6">2 ：
+                                <span style="font-weight: 600">以下、名無しにかわりましてVIPがお送りします</span>
+                                ：2011/03/05(土) 13:41:42.31 ID:OAFBoFRd0
+                            </h5>
+                            <p class="is-size-5">勢いがすごい
+                            </p>
+                        </div>
+                        <div class="my-1">
+                            <a href="#" class="btn btn-primary">続きを読む</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
